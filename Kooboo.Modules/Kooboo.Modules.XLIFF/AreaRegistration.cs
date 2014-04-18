1d9830bf7ddfb30a4934cce16019acfa89e73021
hahqb <hahqb@qq.com>
2014-04-18 16:35:48 +0800

    Add module Kooboo.Modules.XLIFF Upgrade Kooboo CMS

---
+++ Kooboo.Modules/Kooboo.Modules.XLIFF/AreaRegistration.cs
@@ -0,0 +1,50 @@
+﻿using System.Web.Mvc;
+using System.IO;
+using System.Web.Routing;
+using Kooboo.Web.Mvc;
+using Kooboo.CMS.Common;
+using Kooboo.CMS.Account.Services;
+using Kooboo.CMS.Account.Models;
+
+namespace Kooboo.Modules.XLIFF
+{
+    public class AreaRegistration : AreaRegistrationEx
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "XLIFF";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                AreaName,
+                AreaName + "/{controller}/{action}",
+                new { action = "Index" }
+                , null
+                , new[] { "Kooboo.Modules.XLIFF.Controllers", "Kooboo.Web.Mvc", "Kooboo.Web.Mvc.WebResourceLoader" }
+            );
+
+            //Kooboo.Web.Mvc.Menu.MenuFactory.RegisterAreaMenu(AreaName, AreaHelpers.CombineAreaFilePhysicalPath(AreaName, "Menu.config"));
+            Kooboo.Web.Mvc.WebResourceLoader.ConfigurationManager.RegisterSection(AreaName, AreaHelpers.CombineAreaFilePhysicalPath("Sites", "WebResources.config"));
+
+            #region RegisterPermissions
+            var roleManager = Kooboo.CMS.Common.Runtime.EngineContext.Current.Resolve<RoleManager>();
+            roleManager.AddPermission(Permission.Contents_SettingPermission);
+            roleManager.AddPermission(Permission.Contents_SchemaPermission);
+            roleManager.AddPermission(Permission.Contents_FolderPermission);
+            roleManager.AddPermission(Permission.Contents_ContentPermission);
+            roleManager.AddPermission(Permission.Contents_BroadcastingPermission);
+            roleManager.AddPermission(Permission.Contents_WorkflowPermission);
+            roleManager.AddPermission(Permission.Contents_SearchSettingPermission);
+            roleManager.AddPermission(Permission.Contents_HtmlBlockPermission);
+
+            #endregion
+
+            base.RegisterArea(context);
+        }
+    }
+}
---
+++ Kooboo.Modules/Kooboo.Modules.XLIFF/UI/ImportExportToolbarsProvider.cs
@@ -0,0 +1,76 @@
+﻿using Kooboo.CMS.Sites.Extension.UI;
+using Kooboo.CMS.Sites.Extension.UI.TopToolbar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Kooboo.Globalization;
+
+namespace Kooboo.Modules.XLIFF.UI
+{
+    [Kooboo.CMS.Common.Runtime.Dependency.Dependency(typeof(IToolbarProvider))]
+    public class ImportExportToolbarsProvider : IToolbarProvider
+    {
+        private const string GroupName = "XLIFF";
+        private const string AreaName = "XLIFF";
+
+        public MvcRoute[] ApplyTo
+        {
+            get
+            {
+                return new[]{
+                    new MvcRoute(){
+                        Area="Contents",
+                        Controller="TextContent",
+                        Action="Index"
+                    }
+                };
+            }
+        }
+
+        public IEnumerable<ToolbarButton> GetButtons(System.Web.Routing.RequestContext requestContext)
+        {
+            return new ToolbarButton[]{
+                 new ToolbarButton(){
+                    GroupName = GroupName,
+                    CommandTarget = new MvcRoute(){ Action="TextContentImport",Controller="TextContent",Area=AreaName,RouteValues=new Dictionary<string,object>(){
+                        {"Formatter",GroupName}}
+                    },
+                    CommandText="Import".Localize()
+                },
+                //new ToolbarButton(){
+                //    GroupName = GroupName,
+                //    CommandTarget = new MvcRoute(){ Action="Export",Controller="TextContent",Area=AreaName,RouteValues=new Dictionary<string,object>(){
+                //        {"Formatter",GroupName}}
+                //    },
+                //    CommandText="Export current folder".Localize(),
+                //    HtmlAttributes = new Dictionary<string,object>(){
+                //        {"data-command-type","Download"}
+                //    }
+                //},
+                new ToolbarButton(){
+                    GroupName = GroupName,
+                    CommandTarget = new MvcRoute(){ Action="Export",Controller="TextContent",Area=AreaName,RouteValues=new Dictionary<string,object>(){
+                        {"Formatter",GroupName}}
+                    },
+                    CommandText="Export".Localize(),
+                    HtmlAttributes = new Dictionary<string,object>(){
+                        {"data-show-on-check","Any"},
+                        {"data-command-type","Download"}
+                    }
+                }
+           };
+        }
+
+        public IEnumerable<ToolbarGroup> GetGroups(System.Web.Routing.RequestContext requestContext)
+        {
+            return new ToolbarGroup[]{ new ToolbarGroup()
+            {
+                GroupName = GroupName,
+                DisplayText = "XLIFF".Localize(),
+                IconClass="export"
+            }};
+        }
+    }
+}
